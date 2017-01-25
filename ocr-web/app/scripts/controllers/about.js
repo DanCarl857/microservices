8778eda2ec05dbd6a6f2bccbddc1a4c3d4385099
Daniel Carlson <dancarl758@gmail.com>
2017-01-25 11:39:58 +0100

    finished microservices

---
+++ ocr-web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ocrWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ocrWebApp
+ */
+angular.module('ocrWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
